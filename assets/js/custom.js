be1e5b5313c358090ba929aad342f2199d33ca7d
Rajib Sarker <redhat0593@gmail.com>
2023-04-29 14:17:18 +0600

    slider added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,1 @@
 // form file add
-const addButton = document.getElementById('addMoreFile');
-
-addButton.addEventListener('click', () => {
-    const filesParent = document.getElementById('fileHolder');
-
-    const child = document.createElement('input')
-    child.type = 'file'
-    child.classList.add('form-control')
-    child.classList.add('file-input')
-    filesParent.appendChild(child);
-})
